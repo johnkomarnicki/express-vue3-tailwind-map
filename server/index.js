2c002794d7486f6bb63431e16fd4fdd69c205574
JohnRay1997 <johnkomarnickicontact@gmail.com>
2021-12-27 08:53:50 -0500

    section-1
    create server: install dependencies, create scripts and setup basic express server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+
+app.listen(PORT, () => console.log(`app started on port: ${PORT}`));
