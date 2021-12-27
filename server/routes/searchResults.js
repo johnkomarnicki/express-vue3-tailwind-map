19be5e29e0a6ea75d6fd87c660cdcde2a1084da7
JohnRay1997 <johnkomarnickicontact@gmail.com>
2021-12-27 09:25:14 -0500

    section-3
    add url search params: add variable for search params to make our call cleaner & accept numerous amounts of search params

--- server/routes/searchResults.js
+++ server/routes/searchResults.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const url = require("url");
 
 router.get("/:query", async (req, res) => {
   try {
+    // adding api key & query strings
+    const params = new URLSearchParams({
+      access_token: process.env.API_KEY,
+      ...url.parse(req.url, true).query,
+    });
     // getting query param from req
     const query = req.params.query;
     const results = await axios(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${process.env.API_KEY}`
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`
     );
     const data = results.data;
     res.status(200).json(data);
