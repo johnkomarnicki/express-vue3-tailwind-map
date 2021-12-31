b428556c66066026c38e2b3bb4c5726ef9312cdf
JohnRay1997 <johnkomarnickicontact@gmail.com>
2021-12-31 11:00:33 -0500

    section-6
    display map & styles for it, add .env to client for access key for map

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,10 +1,39 @@
 <template>
-  <div class="home"></div>
+  <div class="h-screen relative">
+    <div id="mapid" class="h-full z-[1]"></div>
+  </div>
 </template>
 
 <script>
+import leaflet from "leaflet";
+import { onMounted } from "vue";
 export default {
   name: "HomeView",
   components: {},
+  setup() {
+    let mymap;
+    onMounted(() => {
+      // init map
+      mymap = leaflet
+        .map("mapid", {
+          zoomControl: false,
+        })
+        .setView([28.538336, -81.379234], 10);
+
+      // add tile layers
+      leaflet
+        .tileLayer(
+          `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${process.env.VUE_APP_API_KEY}`,
+          {
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: process.env.VUE_APP_API_KEY,
+          }
+        )
+        .addTo(mymap);
+    });
+  },
 };
 </script>
