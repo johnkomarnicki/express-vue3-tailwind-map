23b50860a0c1fc3c12825be7eaa5bc26bfa08eba
JohnRay1997 <johnkomarnickicontact@gmail.com>
2022-01-01 18:28:16 -0500

    section-7
    get users geolocation & plot the location on the map

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -6,15 +6,15 @@
 
 <script>
 import leaflet from "leaflet";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 export default {
   name: "HomeView",
   components: {},
   setup() {
-    let mymap;
+    let map;
     onMounted(() => {
       // init map
-      mymap = leaflet
+      map = leaflet
         .map("mapid", {
           zoomControl: false,
         })
@@ -32,8 +32,59 @@ export default {
             accessToken: process.env.VUE_APP_API_KEY,
           }
         )
-        .addTo(mymap);
+        .addTo(map);
+
+      // get users location
+      getGelocation();
     });
+
+    const coords = ref(null);
+    const fetchCoords = ref(null);
+    const geoMarker = ref(null);
+
+    const getGelocation = () => {
+      fetchCoords.value = true;
+      navigator.geolocation.getCurrentPosition(setCoords, getLocError);
+    };
+
+    const setCoords = (pos) => {
+      // stop fetching
+      fetchCoords.value = null;
+
+      // set coords in session storage
+      const sessionCoords = {
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude,
+      };
+      sessionStorage.setItem("coords", JSON.stringify(setCoords));
+
+      // set ref coords value
+      coords.value = sessionCoords;
+
+      plotGeoLocation(coords);
+    };
+
+    const getLocError = (error) => {
+      // stop fetching coords
+      fetchCoords.value = null;
+      console.warn(error);
+    };
+
+    const plotGeoLocation = (coords) => {
+      // create custom marker
+      const customMarker = leaflet.icon({
+        iconUrl: require("../assets/map-marker-red.svg"),
+        iconSize: [35, 35],
+      });
+
+      // create new marker with coords and custom marker
+      geoMarker.value = leaflet
+        .marker([coords.value.lat, coords.value.lng], { icon: customMarker })
+        .addTo(map);
+
+      // set map view to current location
+      map.setView([coords.value.lat, coords.value.lng], 10);
+    };
   },
 };
 </script>
