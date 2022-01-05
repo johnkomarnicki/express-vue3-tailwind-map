875befdcbb7995032a2d8410b373dac2e45770e5
JohnRay1997 <johnkomarnickicontact@gmail.com>
2022-01-05 15:47:54 -0500

    section-9

--- client/src/components/MapFeatures.vue
+++ client/src/components/MapFeatures.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex gap-4 px-6 py-8 bg-transparent md:px-0 md:py-0">
+  <div
+    class="w-full md:w-auto absolute md:top-[40px] md:left-[60px] z-[2] flex gap-4 px-6 py-8 md:px-0 md:py-0 bg-transparent"
+  >
     <!-- Geolocation -->
     <div
       class="px-4 bg-white flex items-center shadow-md rounded-md min-h-[45px]"
@@ -7,8 +9,8 @@
       :class="{ 'bg-slate-600': coords }"
     >
       <i
-        class="fas fa-location-arrow text-[18px]"
-        :class="{ 'text-white': coords, 'text-slate-600': !coords }"
+        class="fas fa-location-arrow 'text-slate-600' text-[18px]"
+        :class="{ 'text-white': coords, 'animate-pulse': fetchCoords }"
       ></i>
     </div>
   </div>
